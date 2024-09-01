16bfcec185585e6c07227e2f6300e166ee1fd198
tzafrirhn <chen.t@wayo.co.il>
2024-09-01 18:22:38 +0300

    Update _genonce.sh

--- T17/_genonce.sh
+++ T17/_genonce.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Increase heap size to 32GB and set an initial heap size of 8GB
-export JAVA_OPTS="-Xms8g -Xmx32g"
-
 publisher_jar=publisher.jar
 input_cache_path=./input-cache/
 echo Checking internet connection...
@@ -19,6 +16,11 @@ fi
 echo "$txoption"
 
 export JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS -Dfile.encoding=UTF-8"
+
+# Set the maximum heap size to 4GB (adjust as necessary)
+JAVA_HEAP_SIZE="-Xmx4g"
+# Additional memory options
+JAVA_GC_OPTIONS="-XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./heap-dump.hprof"
 
 publisher=$input_cache_path/$publisher_jar
 if test -f "$publisher"; then
